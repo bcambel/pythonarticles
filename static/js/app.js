4e90648d489ed2b2f6d8c4ef57e46494496d8e56
Bahadir Cambel <bcambel@gmail.com>
2013-07-27 11:26:05 +0200

    regen, generate left menu

--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,28 @@
 (function($){
+	var $leftMenu, $article, html;
 	$(init);
 	function init(){
 		// $(".articles").masonry({
 		// 						   itemSelector : 'a',
 		// 						   columnWidth : 230
 		// 					   });
+		$article = $('article');
+		$leftMenu = $('#left-menu');
+		if($leftMenu.length>0){
+			renderLeftMenu();
+		}
+	}
+
+	function renderLeftMenu(){
+		html = [];
+		$('h2',$article).each(function(i,x){
+			html.push("<li><a href='#section");
+			html.push(i);
+			html.push("'>")
+			html.push($(x).text());
+			html.push("</a></li>");
+			$(x).attr("id","section"+i.toString());
+		});
+		$("ul",$leftMenu).html(html.join(""));
 	}
 })(jQuery);
